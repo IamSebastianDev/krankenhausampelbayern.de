7bf9025affa757d612c62f7d8d66735b3311e305
Sebastian <sebastian@iamsebastian.dev>
2022-01-26 23:36:22 +0100

    PROG: Added TailwindCSS

---
+++ dashboard/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @format */
+
+module.exports = {
+	darkMode: 'class',
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				nunito: ['nunito', 'system-ui', 'sans-serif'],
+			},
+		},
+	},
+	plugins: [],
+};
