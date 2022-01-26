c21a65e8eef4660f3940f10218ad27becfa51cc9
Sebastian <sebastian@iamsebastian.dev>
2022-01-26 14:20:47 +0100

    inital commit for dashboard

---
+++ dashboard/src/index.js
@@ -0,0 +1,12 @@
+/** @format */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './resets.css';
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+);
