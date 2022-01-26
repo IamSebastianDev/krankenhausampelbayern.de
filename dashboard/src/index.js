b958a1e7627123f1e18d239ec55341b1db3655f0
Sebastian <sebastian@iamsebastian.dev>
2022-01-26 23:37:03 +0100

    PROG: Added themectx, Navigation

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -3,10 +3,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './resets.css';
+import './index.css';
+
+import { ThemeContext, ThemeCtx } from './store/theme.context';
+
+import App from './App.js';
+import { Navigation } from './components/Navigation/Navigation';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<ThemeContext>
+			<Navigation />
+			<App />
+		</ThemeContext>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
