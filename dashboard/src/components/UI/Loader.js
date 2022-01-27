45dc3fe5fd30880cd80785363c5d4ce8a256ca4b
Sebastian <sebastian@iamsebastian.dev>
2022-01-27 18:09:06 +0100

    FEAT: Added loader component

---
+++ dashboard/src/components/UI/Loader.js
@@ -0,0 +1,43 @@
+/** @format */
+
+import React, { useEffect, useState } from 'react';
+import { joinClassNames as cls } from '../../scripts/joinClassNames.util';
+
+const Spinner = ({ className }) => {
+	return (
+		<span
+			className={cls(
+				'absolute w-20 h-20 rounded-full animate-spin',
+				className
+			)}
+		/>
+	);
+};
+
+export const Loader = ({ loading }) => {
+	const [visible, setVisible] = useState(loading);
+	const [animate, setAnimate] = useState(false);
+
+	useEffect(() => {
+		if (!loading) {
+			setAnimate(true);
+			window.setTimeout(() => {
+				setVisible(false);
+			}, 1000);
+		}
+	}, [loading]);
+
+	return (
+		<div
+			className={cls(
+				'w-screen h-screen fixed top-0 left-0 flex-col justify-center items-center bg-zinc-200 dark:bg-zinc-900 transition-transopaque duration-1000 transform z-40',
+				visible ? 'flex' : 'hidden',
+				animate ? ' scale-150 opacity-0' : 'scale-100 opacity-100'
+			)}>
+			<Spinner className="border-r-4 border-r-blue-500" />
+			<Spinner className="border-t-4 border-t-sky-500" />
+			<Spinner className="border-l-4 border-l-teal-500" />
+			<Spinner className="border-b-4 border-b-cyan-500" />
+		</div>
+	);
+};
