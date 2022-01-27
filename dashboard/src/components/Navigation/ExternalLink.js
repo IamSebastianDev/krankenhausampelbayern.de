f3c019fdf8199781eeaad4a6d88a9b49bab0d214
Sebastian <sebastian@iamsebastian.dev>
2022-01-27 18:09:59 +0100

    PROG: Refractored Navigation components

---
+++ dashboard/src/components/Navigation/ExternalLink.js
@@ -0,0 +1,25 @@
+/** @format */
+
+import { forwardRef } from 'react';
+import { joinClassNames as cls } from '../../scripts/joinClassNames.util';
+
+export const ExternalLink = forwardRef(
+	({ href, title, target = '_blank', className, children }, ref) => {
+		return (
+			<li
+				className={cls(
+					'hover:text-blue-500  px-1 mx-2 flex flex-row justify-center items-center',
+					className
+				)}>
+				<a
+					ref={ref}
+					href={href}
+					title={title}
+					rel="noreferrer noopener"
+					target={target}>
+					{children}
+				</a>
+			</li>
+		);
+	}
+);
--- dashboard/src/components/Navigation/InternalLink.js
+++ dashboard/src/components/Navigation/InternalLink.js
@@ -2,23 +2,23 @@
 
 import { forwardRef } from 'react';
 import { joinClassNames as cls } from '../../scripts/joinClassNames.util';
+import { NavLink } from 'react-router-dom';
 
-export const NavLink = forwardRef(
-	({ href, title, target = '_blank', className, children }, ref) => {
+export const InternalLink = forwardRef(
+	({ to, title, className, children }, ref) => {
 		return (
 			<li
 				className={cls(
 					'hover:text-blue-500  px-1 mx-2 flex flex-row justify-center items-center',
 					className
 				)}>
-				<a
+				<NavLink
 					ref={ref}
-					href={href}
 					title={title}
 					rel="noreferrer noopener"
-					target={target}>
+					to={to}>
 					{children}
-				</a>
+				</NavLink>
 			</li>
 		);
 	}
