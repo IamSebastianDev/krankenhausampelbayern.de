9a1827bc5e5867164a25877e4339f767f7f73ec6
Sebastian <sebastian@iamsebastian.dev>
2022-01-27 18:10:15 +0100

    COMP: Added a navigation for mobile

--- dashboard/src/components/Navigation/Navigation.js
+++ dashboard/src/components/Navigation/Navigation.js
@@ -1,63 +1,58 @@
 /** @format */
 
-import React, { useContext, useState } from 'react';
+import React, { useState, useContext } from 'react';
+import { DataCtx } from '../../store/data.context';
 
-import { NavLink } from './NavLink';
-import { NavButton } from './NavButton';
-import { Github, SunBig, MoonStylized, Menu } from 'react-pangolicons';
-import { ThemeModal } from './ThemeModal';
-import { ThemeCtx } from '../../store/theme.context';
+import { Github, X, Menu, InfoCircle } from 'react-pangolicons';
+
+import { ExternalLink } from './ExternalLink';
+import { InternalLink } from './InternalLink';
+import { ThemeSelector } from './ThemeSelector';
+import { MobileNav } from './MobileNav';
 
 export const Navigation = () => {
-	const { theme } = useContext(ThemeCtx);
-	
-	const [showThemeModal, setShowThemeModal] = useState(false);
-	const toggleThemeModal = () => {
-		setShowThemeModal((curState) => !curState);
-	};
+	const { currentEntry } = useContext(DataCtx);
+
+	const [showNav, setShowNav] = useState(false);
+	const toggleNav = () => setShowNav((s) => !s);
+	const navIcon = [
+		<X size={20} className="dark:stroke-1 stroke-2" />,
+		<Menu size={20} className="dark:stroke-1 stroke-2" />,
+	];
 
 	return (
-		<nav className="fixed w-screen dark:bg-zinc-900 shadow border-b border-b-zinc-300 dark:border-b-zinc-700 font-nunito text-lg tracking-wide">
-			<ul className="relative w-full max-w-screen-xl flex flex-row px-2 xl:px-0 py-4 mx-auto dark:text-zinc-100  text-zinc-700 font-bold">
-				<NavLink className="mr-auto">🚦 Covid-19 Bayern</NavLink>
-				<NavLink
-					className="hidden sm:block"
-					href="https://github.com/iamsebastiandev/krankenhausampelbayern.de/widget">
+		<nav className="fixed top-0 w-screen bg-zinc-200 dark:bg-zinc-900 shadow border-b border-b-zinc-300 dark:border-b-zinc-700 dark:text-zinc-100  text-zinc-700 font-nunito text-lg tracking-wide z-50 font-bold">
+			<ul className="relative w-full max-w-screen-xl flex flex-row px-2 xl:px-0 py-4 mx-auto z-20">
+				<InternalLink className="mr-auto" to="/">
+					🚦 Covid-19 Bayern
+				</InternalLink>
+				<InternalLink className="hidden md:flex" to="/">
 					Dashboard
-				</NavLink>
-				<NavLink
-					className="hidden sm:block"
-					href="https://github.com/iamsebastiandev/krankenhausampelbayern.de/widget">
+				</InternalLink>
+				<InternalLink className="hidden md:flex" to="/interface">
 					API
-				</NavLink>
-				<NavLink
-					className="hidden sm:block"
+				</InternalLink>
+				<ExternalLink
+					className="hidden md:flex"
 					href="https://github.com/iamsebastiandev/krankenhausampelbayern.de/widget">
 					Widget <span className="text-xs self-end">für iOs</span>
-				</NavLink>
-				<NavButton className="sm:hidden">
-					<Menu size={20} className="dark:stroke-1 stroke-2" />
-				</NavButton>
+				</ExternalLink>
+				<li className="hover:text-blue-500  px-1 mx-2 flex md:hidden flex-row justify-center items-center">
+					<button onClick={toggleNav}>
+						{navIcon[showNav ? 0 : 1]}
+					</button>
+				</li>
 				<li className="border-l border-l-zinc-300 dark:border-l-zinc-700 mx-2"></li>
-				<NavButton onClick={toggleThemeModal}>
-					{theme === 'dark' ? (
-						<SunBig size={20} className="dark:stroke-1 stroke-2" />
-					) : (
-						<MoonStylized
-							size={20}
-							className="dark:stroke-1 stroke-2"
-						/>
-					)}
-				</NavButton>
-				<NavLink
+				<ThemeSelector />
+				<ExternalLink
 					href="https://github.com/iamsebastiandev/krankenhausampelbayern.de"
 					rel="noreferrer noopener"
 					title="Github"
 					target="_blank">
 					<Github size={20} className="dark:stroke-1 stroke-2" />
-				</NavLink>
+				</ExternalLink>
 			</ul>
-			{showThemeModal && <ThemeModal toggleModal={toggleThemeModal} />}
+			<MobileNav toggleNav={toggleNav} showNav={showNav} />
 		</nav>
 	);
 };
