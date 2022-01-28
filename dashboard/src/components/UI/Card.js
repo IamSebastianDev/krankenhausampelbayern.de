50ddc7eb41902a9322e7e201a864c6b859f277c6
Sebastian <sebastian@iamsebastian.dev>
2022-01-28 19:50:29 +0100

    PROG: Updated Card styles

--- dashboard/src/components/UI/Card.js
+++ dashboard/src/components/UI/Card.js
@@ -1,5 +1,13 @@
 /** @format */
 
-export const Card = ({ children }) => (
-	<div className=" bg-zinc-300 dark:bg-zinc-800">{children}</div>
+import { joinClassNames as cls } from '../../scripts/joinClassNames.util';
+
+export const Card = ({ className, children }) => (
+	<div
+		className={cls(
+			'bg-zinc-100 dark:bg-zinc-900 p-6 rounded-2xl shadow-md border dark:border-zinc-800 border-zinc-300',
+			className
+		)}>
+		{children}
+	</div>
 );
