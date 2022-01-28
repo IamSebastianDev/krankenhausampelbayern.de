ef28d6f2dc7b5d863159455dd209946f3a89df00
Sebastian <sebastian@iamsebastian.dev>
2022-01-28 19:49:16 +0100

    PROG: Added temporary widget display and converter method

--- dashboard/src/components/Dashboard/Dashboard.js
+++ dashboard/src/components/Dashboard/Dashboard.js
@@ -1,22 +1,57 @@
 /** @format */
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { DataCtx } from '../../store/data.context';
 
 import { Section } from '../UI/Section';
 import { Loader } from '../UI/Loader';
-import { Card } from '../UI/Card';
+
+import { Widget } from './Widgets/Widget';
 
 export const Dashboard = () => {
+	const [widgetData, setWidgetData] = useState();
 	const { data, currentEntry } = useContext(DataCtx);
-	const loading = data === undefined;
-	console.log({ data, currentEntry });
+	const loading = widgetData === undefined;
+
+	const theme = {
+		positive: {
+			positive: 'dark:text-blue-500 text-blue-600',
+			neutral: 'dark:text-inherit text-slate-700',
+			negative: 'dark:text-red-500 text-red-600',
+		},
+		negative: {
+			positive: 'dark:text-red-500 text-red-600',
+			neutral: 'dark:text-inherit text-slate-700',
+			negative: 'dark:text-blue-500 text-blue-600',
+		},
+	};
+
+	useEffect(() => {
+		if (currentEntry) {
+			const convertData = Object.entries(currentEntry)
+				.filter(([name, data]) => name !== 'index' && name !== 'meta')
+				.map(([name, data]) => ({
+					...data,
+					theme: theme[
+						name !== 'vaccinated' ? 'negative' : 'positive'
+					],
+				}));
+			setWidgetData(convertData);
+		}
+	}, [currentEntry]);
+	console.log({ widgetData });
 
 	return (
-		<Section id="dashboard">
-			<Loader loading={loading} />
-			{data && (
-				<div className="flex flex-col sm:grid sm:grid-cols-2 md:grid-cols-3 py-20 max-w-screen-xl m-4 xl:mx-auto p-4 border border-blue-500"></div>
+		<Section id="dashboard" className="flex flex-col">
+			<Loader loading={loading} id="main-loader" />
+			{widgetData && (
+				<div className="mx-auto max-w-xs md:max-w-screen-md lg:max-w-screen-lg w-full my-auto">
+					<div className="flex flex-col md:grid md:grid-cols-2 lg:grid-cols-3  my-4 mx-3 md:mx-16 lg:mx-4  gap-6 dark:text-zinc-100 text-zinc-700">
+						{widgetData.map((data, index) => (
+							<Widget data={data} key={index} />
+						))}
+					</div>
+				</div>
 			)}
 		</Section>
 	);
