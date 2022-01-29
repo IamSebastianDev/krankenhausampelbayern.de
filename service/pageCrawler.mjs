9688607de8292349aa66e6acc18fd37dfd8aea74
Sebastian <sebastian@iamsebastian.dev>
2022-01-29 15:28:15 +0100

    FIX: Updated workorder

--- service/pageCrawler.mjs
+++ service/pageCrawler.mjs
@@ -55,15 +55,27 @@ const pageCrawler = async ({ page, workOrder }) => {
 
 // the object containing the different data points and methods to access them.
 
-const workOrder = {
+/** @format */
+
+/**
+ * Due to the LGL Bayern constantly changing the layout of the page, a more flexible crawler is needed to guarantee
+ * fast adapabilty. The object below is used to extract the expected data from the DOM in a modular way. While the
+ * workorder in itslef is pretty boilerplatey, it enables us to quickly adapt to breaking layout changes.
+ *
+ * The workorder will return an object with two properties, data and src. The data property contains the crawled raw
+ * data that will need to be parsed and cleaned, as well as two date strings which serve as src timestamps.
+ *
+ * To create a new order, add a object to the respective property. It needs to contain  a valid CSS
+ * selector to serach for and a callback that will be executed on that selector. The data returned by the
+ * callback will be data point added to the results object.
+ */
+
+ export const workOrder = {
 	data: {
 		hospitalized: {
-			category: 'data',
 			selector: '.jumbotron',
 			callback: (nodeList) => {
-				// get the second jumbotron element
-
-				const elem = nodeList[3];
+				const elem = nodeList[4];
 
 				const dataPoint = elem
 					.querySelectorAll('.horizontal_zwei dd')[1]
@@ -75,22 +87,18 @@ const workOrder = {
 			},
 		},
 		hospitalized7Days: {
-			category: 'data',
 			selector: '#kennzahlen2 .card strong',
 			callback: (nodeList) => {
 				return nodeList[0].textContent.trim().split(/\n/gim)[0].trim();
 			},
 		},
 		hospitalizedIncidence: {
-			category: 'data',
 			selector: '.jumbotron',
 			callback: (nodeList) => {
-				// get the second jumbotron element
-
-				const elem = nodeList[2];
+				const elem = nodeList[3];
 
 				const dataPoint = elem
-					.querySelectorAll('.horizontal_vier dd')[0]
+					.querySelectorAll('.horizontal_zwei dd')[0]
 					.textContent.trim()
 					.split(/\n/gim)[0]
 					.trim();
@@ -99,19 +107,15 @@ const workOrder = {
 			},
 		},
 		icuOccupation: {
-			category: 'data',
 			selector: '#kennzahlen2 .card strong',
 			callback: (nodeList) => {
 				return nodeList[1].textContent.trim().split(/\n/gim)[0].trim();
 			},
 		},
 		cases7Days: {
-			cateogry: 'data',
 			selector: '.jumbotron',
 			callback: (nodeList) => {
-				// get the second jumbotron element
-
-				const elem = nodeList[4];
+				const elem = nodeList[5];
 
 				const dataPoint = elem
 					.querySelectorAll('.horizontal_zwei dd')[1]
@@ -123,15 +127,12 @@ const workOrder = {
 			},
 		},
 		incidence7Days: {
-			cateogry: 'data',
 			selector: '.jumbotron',
 			callback: (nodeList) => {
-				// get the second jumbotron element
-
-				const elem = nodeList[4];
+				const elem = nodeList[5];
 
 				const dataPoint = elem
-					.querySelectorAll('.horizontal_vier dd')[0]
+					.querySelectorAll('.horizontal_zwei dd')[2]
 					.textContent.trim()
 					.split(/\n/gim)[0]
 					.trim();
@@ -140,12 +141,9 @@ const workOrder = {
 			},
 		},
 		rvalue: {
-			cateogry: 'data',
 			selector: '.jumbotron',
 			callback: (nodeList) => {
-				// get the second jumbotron element
-
-				const elem = nodeList[4];
+				const elem = nodeList[5];
 
 				const dataPoint = elem
 					.querySelectorAll('.horizontal_zwei dd')[0]
@@ -156,13 +154,10 @@ const workOrder = {
 				return dataPoint;
 			},
 		},
-		vaccination: {
-			cateogry: 'data',
+		vaccinated: {
 			selector: '.jumbotron',
 			callback: (nodeList) => {
-				// get the second jumbotron element
-
-				const elem = nodeList[5];
+				const elem = nodeList[6];
 
 				const dataPoint = elem
 					.querySelectorAll('.horizontal_vier dd')[0]
@@ -177,19 +172,18 @@ const workOrder = {
 	src: {
 		hospitalized: {
 			selector: '.card .bildunterschrift',
-			category: 'sources',
 			callback: (nodeList) => {
 				return nodeList[0].textContent.split(/\n/gim)[0].trim();
 			},
 		},
 		icuOccupation: {
 			selector: '.card .bildunterschrift',
-			category: 'sources',
 			callback: (nodeList) => {
 				return nodeList[1].textContent.split(/\n/gim)[0].trim();
 			},
 		},
 	},
 };
+
 
 export { pageCrawler, workOrder };
