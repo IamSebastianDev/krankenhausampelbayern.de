b96518e7bb11ca8254a4b7c06432c5100fb81389
Sebastian <sebastian@iamsebastian.dev>
2022-01-29 21:36:51 +0100

    PROG: Updated card component

--- dashboard/src/components/UI/Card.js
+++ dashboard/src/components/UI/Card.js
@@ -5,7 +5,7 @@ import { joinClassNames as cls } from '../../scripts/joinClassNames.util';
 export const Card = ({ className, children }) => (
 	<div
 		className={cls(
-			'bg-zinc-100 dark:bg-zinc-900 p-6 rounded-2xl shadow-md border dark:border-zinc-800 border-zinc-300',
+			'bg-zinc-100 dark:bg-zinc-900 p-6 rounded-2xl shadow-md border dark:border-zinc-800 border-zinc-300 dark:bg-opacity-50 bg-opacity-50 backdrop-blur-md',
 			className
 		)}>
 		{children}
