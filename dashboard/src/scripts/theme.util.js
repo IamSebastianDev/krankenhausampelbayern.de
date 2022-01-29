30c75f09182ea6060b918403bf6d571184f9eafc
Sebastian <sebastian@iamsebastian.dev>
2022-01-29 21:38:05 +0100

    FEAT: Refractored textsizes and theme into utility objects

---
+++ dashboard/src/scripts/theme.util.js
@@ -0,0 +1,14 @@
+/** @format */
+
+export const theme = {
+	normal: {
+		positive: 'dark:text-blue-500 text-blue-600',
+		neutral: 'dark:text-inherit text-slate-700',
+		negative: 'dark:text-red-500 text-red-600',
+	},
+	inverted: {
+		positive: 'dark:text-red-500 text-red-600',
+		neutral: 'dark:text-inherit text-slate-700',
+		negative: 'dark:text-blue-500 text-blue-600',
+	},
+};
