ec78d25d3aadeebedb5ed84842acf292ea287382
Sebastian <sebastian@iamsebastian.dev>
2022-01-29 21:37:36 +0100

    FEAT: Added error extraction to useData hook

--- dashboard/src/hooks/useData.hook.js
+++ dashboard/src/hooks/useData.hook.js
@@ -1,23 +1,34 @@
 /** @format */
 
-import React, { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 import { adaptData } from 'c19api-adapternode';
 
 export const useData = () => {
 	const [data, setData] = useState();
+	const [error, setError] = useState();
 
 	const fetchData = useCallback(async () => {
 		const res = await fetch('/api');
-		setData(adaptData(await res.json()));
+		const parsed = await res.json();
+
+		if (parsed.error) {
+			setError(parsed.setError);
+		}
+
+		setData(adaptData(parsed));
 	}, []);
 
 	useEffect(() => {
 		if (!data) {
-			fetchData();
+			try {
+				fetchData();
+			} catch (e) {
+				setError(e);
+			}
 		}
 	}, [data, fetchData]);
 
 	const currentEntry = data ? data.history[data.history.length - 1] : null;
 
-	return { data, currentEntry };
+	return { data, currentEntry, error };
 };
